99ab77710254a75bdbe1412895386937d271d78f
Alexander Schulz <alex.transporter@gmail.com>
2022-01-05 12:15:27 +0100

    CODE-133: functionality to write to sheets added with unit tests.

--- sous_chef/tests/unit_tests/test_gsheets_api.py
+++ sous_chef/tests/unit_tests/test_gsheets_api.py
@@ -24,7 +24,7 @@ class TestGSheetsHelper:
                 and f["mimeType"] == "application/vnd.google-apps.folder"
             )
         ]
-        assert len(found) == 1
+        assert len(found) == 1, "Could not verify valid google drive/sheets connection!"
 
     @pytest.mark.parametrize(
         "test_workbook, test_sheet, expected_df",
@@ -39,4 +39,28 @@ class TestGSheetsHelper:
     def test_get_sheet_as_df(self, test_workbook, test_sheet, expected_df):
         assert np.all(
             gsheets_helper.get_sheet_as_df(test_workbook, test_sheet) == expected_df
+        ), "Could not validate reading of test df!"
+
+    @pytest.mark.parametrize(
+        "test_workbook, test_sheet, write_df",
+        [
+            (
+                "unit_test_sheet",
+                "test_sheet_2",
+                pd.DataFrame(
+                    {
+                        "Vars": ["a", "b", "b", "d"],
+                        "Vals": [1, 2, 3, pd.datetime.now().strftime("%H%M%S")],
+                    }
+                ),
+            )
+        ],
+    )
+    def test_write_df_to_sheet(self, test_workbook, test_sheet, write_df):
+        write_df["Vals"] = write_df["Vals"].astype(int)
+        gsheets_helper.write_df_to_sheet(
+            write_df, test_sheet, test_workbook, copy_index=False
         )
+        assert np.all(
+            gsheets_helper.get_sheet_as_df(test_workbook, test_sheet) == write_df
+        ), "Could not validate writing of test dataframe to Google drive!"

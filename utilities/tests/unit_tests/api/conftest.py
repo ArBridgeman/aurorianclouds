1fff06e2db10f2528a33cbad06062bc19d9a85ca
Ariel Schulz <ariel.bridgem@gmail.com>
2024-03-25 20:06:43 +0100

    Separate unit tests into inherited test structure

--- utilities/tests/unit_tests/api/conftest.py
+++ utilities/tests/unit_tests/api/conftest.py
@@ -9,9 +9,3 @@ from utilities.api.todoist_api import TodoistHelper
 def mock_todoist_helper(todoist_config):
     with patch.object(TodoistHelper, TodoistHelper.__post_init__.__name__):
         return TodoistHelper(todoist_config)
-
-
-# fixture to iterate over other fixtures
-@pytest.fixture(params=["mock_todoist_helper", "debug_todoist_helper"])
-def todoist_helper(request):
-    return request.getfixturevalue(request.param)
--- utilities/tests/unit_tests/api/test_todoist_api.py
+++ utilities/tests/unit_tests/api/test_todoist_api.py
@@ -22,11 +22,17 @@ from utilities.api.base_classes.todoist import (
         ),
     ],
 )
-def test_get_due_date_str(todoist_helper, due_date, string):
+def test_get_due_date_str(due_date, string):
     assert get_due_datetime_str(due_date) == string
 
 
 class TestTodoistHelper:
+    @staticmethod
+    @pytest.fixture
+    def implementation(debug_todoist_helper, default_project):
+        # used to determine which fixture is being used
+        yield debug_todoist_helper
+
     @staticmethod
     @pytest.mark.parametrize(
         "label,cleaned_label",
@@ -40,24 +46,32 @@ class TestTodoistHelper:
             ("pasta___broccoli", "pasta_broccoli"),
         ],
     )
-    def test__clean_label(todoist_helper, label, cleaned_label):
-        assert todoist_helper._clean_label(label) == cleaned_label
+    def test__clean_label(implementation, label, cleaned_label):
+        assert implementation._clean_label(label) == cleaned_label
 
     @staticmethod
     @pytest.mark.parametrize(
         "due_date",
         [None, 42, "2022-02-22"],
     )
-    def test__get_due_date_str_raise_attribute_error(todoist_helper, due_date):
+    def test__get_due_date_str_raise_attribute_error(implementation, due_date):
         with pytest.raises(AttributeError):
-            todoist_helper._get_due_datetime_str(due_date)
+            implementation._get_due_datetime_str(due_date)
 
     @staticmethod
-    def test_get_project_id_if_not_exists(todoist_helper):
-        todoist_helper.projects = {}
+    def test_get_project_id_if_not_exists(implementation):
+        implementation.projects = {}
         with pytest.raises(TodoistKeyError) as error:
-            todoist_helper.get_project_id("not-a-project-name")
+            implementation.get_project_id("not-a-project-name")
         assert str(error.value) == (
             "[todoist key error]: tag=project_id for "
             "value=not-a-project-name"
         )
+
+
+class TestMockTodoistHelper(TestTodoistHelper):
+    @staticmethod
+    @pytest.fixture
+    def implementation(mock_todoist_helper, default_project):
+        # used to determine which fixture is being used
+        yield mock_todoist_helper

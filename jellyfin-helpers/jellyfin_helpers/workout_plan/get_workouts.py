abbe34b3f0940825943888b90df316b532cb698f
Ariel Schulz <ariel.bridgem@gmail.com>
2024-10-07 19:31:06 +0200

    Add difficulty & rating from tags to workout cache

--- jellyfin-helpers/jellyfin_helpers/workout_plan/get_workouts.py
+++ jellyfin-helpers/jellyfin_helpers/workout_plan/get_workouts.py
@@ -4,7 +4,7 @@ from typing import List, Tuple
 
 import pandas as pd
 from jellyfin_helpers.jellyfin_api import Jellyfin
-from jellyfin_helpers.workout_plan.models import WorkoutVideoSchema
+from jellyfin_helpers.workout_plan.models import Difficulty, WorkoutVideoSchema
 from joblib import Memory
 from pandera.typing.common import DataFrameBase
 from structlog import get_logger
@@ -22,18 +22,56 @@ class WorkoutVideos:
     def __init__(self, jellyfin: Jellyfin):
         self.jellyfin = jellyfin
 
+    def parse_workout_videos(self):
+        return _parse_workout_videos(jellyfin=self.jellyfin)
+
+
+class TagExtractor:
     @staticmethod
-    def get_tool_from_tags(tags: List[str]) -> str:
-        if not isinstance(tags, list):
-            raise ValueError("tags must be a list of strings")
+    def _get_difficulty_from_tags(tags: List[str]) -> Tuple[str, int]:
+        difficulty_str = "difficulty/"
+        difficulty = list(filter(lambda x: difficulty_str in x, tags))
+        if len(difficulty) > 1:
+            raise ValueError(
+                f"difficulty can only have 1 value, but was {difficulty}"
+            )
+        # TODO panderas cannot handle enums properly so double-cast :/
+        elif len(difficulty) == 1:
+            difficulty = difficulty[0].replace(difficulty_str, "")
+            difficulty_enum = Difficulty[difficulty]
+            return difficulty_enum.name, difficulty_enum.value
+        return Difficulty.normal.name, Difficulty.normal.value
+
+    @staticmethod
+    def _get_rating_from_tags(tags: List[str]) -> float:
+        rating_str = "stars/"
+        rating = list(filter(lambda x: rating_str in x, tags))
+        if len(rating) > 1:
+            raise ValueError(f"rating can only have 1 value, but was {rating}")
+        elif len(rating) == 1:
+            rating = rating[0].replace(rating_str, "")
+            return float(rating)
+        return 3.0
+
+    @staticmethod
+    def _get_tools_from_tag(tags: List[str]) -> str:
         tool_str = "tool/"
         tools = list(filter(lambda x: tool_str in x, tags))
         if len(tools) < 1:
             return ""
         return ", ".join(tool.replace(tool_str, "") for tool in tools)
 
-    def parse_workout_videos(self):
-        return _parse_workout_videos(jellyfin=self.jellyfin)
+    @staticmethod
+    def extract_tags(tags: List[str]) -> Tuple[str, int, float, str]:
+        if not isinstance(tags, list):
+            raise ValueError("tags must be a list of strings")
+
+        difficulty, difficulty_num = TagExtractor._get_difficulty_from_tags(
+            tags
+        )
+        rating = TagExtractor._get_rating_from_tags(tags)
+        tools = TagExtractor._get_tools_from_tag(tags)
+        return difficulty, difficulty_num, rating, tools
 
 
 @cache.cache
@@ -52,6 +90,7 @@ def _parse_workout_videos(
     LOGGER.info(f"using cache for {cache_date}")
 
     exercise_genres = jellyfin.get_genres_per_library(library_name=library_name)
+    columns_to_extract = WorkoutVideoSchema.to_schema().columns.keys()
 
     data_frame = pd.DataFrame()
     for genre in exercise_genres:
@@ -67,14 +106,19 @@ def _parse_workout_videos(
             raw_data["RunTimeTicks"] / 10**7 / 60, unit="m"
         )
         raw_data["genre"] = genre["Name"]
-        raw_data["tool"] = raw_data["Tags"].apply(
-            WorkoutVideos.get_tool_from_tags
+        raw_data[
+            ["difficulty", "difficulty_num", "rating", "tool"]
+        ] = raw_data.apply(
+            lambda x: TagExtractor.extract_tags(x["Tags"]),
+            axis=1,
+            result_type="expand",
         )
+
         raw_data.columns = raw_data.columns.str.lower()
 
         data_frame = pd.concat(
             [
-                raw_data[["name", "id", "duration", "genre", "tags", "tool"]],
+                raw_data[columns_to_extract],
                 data_frame,
             ]
         )
--- jellyfin-helpers/tests/unit_tests/workout_plan/test_get_workouts.py
+++ jellyfin-helpers/tests/unit_tests/workout_plan/test_get_workouts.py
@@ -1,5 +1,65 @@
+from typing import List
+
 import pytest
-from jellyfin_helpers.workout_plan.get_workouts import WorkoutVideos
+from jellyfin_helpers.workout_plan.get_workouts import TagExtractor
+from jellyfin_helpers.workout_plan.models import Difficulty
+
+
+class TestGetDifficultyFromTags:
+    @staticmethod
+    @pytest.mark.parametrize(
+        "tags,expectation",
+        [
+            pytest.param([], Difficulty.normal, id="no_difficulty_tag"),
+            pytest.param(
+                ["difficulty/minus"],
+                Difficulty.minus,
+                id="minus_difficulty_tag",
+            ),
+            pytest.param(
+                ["difficulty/plus"], Difficulty.plus, id="plus_difficulty_tag"
+            ),
+            pytest.param(
+                ["difficulty/minus", "random"],
+                Difficulty.minus,
+                id="more_than_one_tag",
+            ),
+        ],
+    )
+    def test_works_as_expected(tags: List[str], expectation: Difficulty):
+        result = TagExtractor._get_difficulty_from_tags(tags)
+        assert result == (expectation.name, expectation.value)
+
+    @staticmethod
+    def test_more_than_one_tag_raises_exception():
+        with pytest.raises(ValueError):
+            TagExtractor._get_difficulty_from_tags(
+                ["difficulty/minus", "difficulty/plus"]
+            )
+
+
+class TestGetRatingFromTags:
+    @staticmethod
+    @pytest.mark.parametrize(
+        "tags,expectation",
+        [
+            pytest.param([], 3.0, id="no_rating_tag"),
+            pytest.param(["stars/2.5"], 2.5, id="rating_float_tag"),
+            pytest.param(["stars/4"], 4, id="rating_int_tag"),
+            pytest.param(
+                ["stars/4", "random"],
+                4,
+                id="more_than_one_tag",
+            ),
+        ],
+    )
+    def test_works_as_expected(tags: List[str], expectation):
+        assert TagExtractor._get_rating_from_tags(tags) == expectation
+
+    @staticmethod
+    def test_more_than_one_tag_raises_exception():
+        with pytest.raises(ValueError):
+            TagExtractor._get_rating_from_tags(["stars/4", "stars/2.5"])
 
 
 class TestGetToolFromTags:
@@ -23,11 +83,57 @@ class TestGetToolFromTags:
             ),
         ],
     )
-    def test_works_as_expected(tags, expectation):
-        assert WorkoutVideos.get_tool_from_tags(tags) == expectation
+    def test_works_as_expected(tags: List[str], expectation):
+        assert TagExtractor._get_tools_from_tag(tags) == expectation
+
+
+class TestTagExtractor:
+    @staticmethod
+    @pytest.mark.parametrize(
+        "tags,exp_difficulty, exp_rating, exp_tools",
+        [
+            pytest.param([], Difficulty.normal, 3.0, "", id="use_default_tags"),
+            pytest.param(
+                ["tool/has-weights"],
+                Difficulty.normal,
+                3.0,
+                "has-weights",
+                id="one_tool_tag",
+            ),
+            pytest.param(
+                ["difficulty/plus"],
+                Difficulty.plus,
+                3.0,
+                "",
+                id="plus_difficulty_tag",
+            ),
+            pytest.param(
+                ["stars/4"], Difficulty.normal, 4.0, "", id="rating_tag"
+            ),
+            pytest.param(
+                ["difficulty/minus", "stars/4", "tool/has-weights"],
+                Difficulty.minus,
+                4.0,
+                "has-weights",
+                id="all_tags",
+            ),
+        ],
+    )
+    def test_works_as_expected(
+        tags: List[str],
+        exp_difficulty: Difficulty,
+        exp_rating: float,
+        exp_tools: str,
+    ):
+        assert TagExtractor.extract_tags(tags) == (
+            exp_difficulty.name,
+            exp_difficulty.value,
+            exp_rating,
+            exp_tools,
+        )
 
     @staticmethod
-    @pytest.mark.parametrize("tags", [1, "1234", True])
-    def test_raise_error_if_not_list(tags):
+    @pytest.mark.parametrize("tag", [1, "1234", True])
+    def test_raise_error_if_not_list(tag):
         with pytest.raises(ValueError):
-            WorkoutVideos.get_tool_from_tags(tags)
+            TagExtractor.extract_tags(tag)
